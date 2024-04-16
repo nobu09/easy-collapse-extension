d541f7341abf97ad9dddfff4ca0054efbe928c0b
nobu09 <11857481+nobu09@users.noreply.github.com>
2024-04-17 08:41:47 +0900

    contextMenuがクリックされた時の処理を仮実装

--- background.js
+++ background.js
@@ -5,3 +5,16 @@ chrome.runtime.onInstalled.addListener(() => {
     contexts: ['editable'] // 編集可能なテキストフィールド上で右クリックしたときに表示
   });
 })
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'pasteCollapsibleSection') {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: pasteCollapsibleSection,
+    })
+  }
+});
+
+function pasteCollapsibleSection() {
+  console.log('pasteCollapsibleSection');
+}
