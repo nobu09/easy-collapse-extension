a16cf5f320a22e4f39b22198e449b64869ca3f9b
nobu09 <11857481+nobu09@users.noreply.github.com>
2024-04-18 09:14:30 +0900

    折りたたみセクションをカーソル位置に挿入する

--- background.js
+++ background.js
@@ -16,5 +16,21 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
 });
 
 function pasteCollapsibleSection() {
-  console.log('pasteCollapsibleSection');
+  let cursor = document.activeElement;
+  if (cursor.tagName !== 'TEXTAREA') return;
+
+  let start = cursor.selectionStart;
+  let end = cursor.selectionEnd;
+  let text = cursor.value;
+
+  let before = text.substring(0, start);
+  let after  = text.substring(end, text.length);
+
+  let collapsibleSection = `<details>
+<summary>Title</summary>
+
+Contents
+</details>`;
+
+  cursor.value = (before + collapsibleSection + after);
 }
